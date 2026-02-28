ebe86907aa4f65842c76293f51456a869c842b81
DarrenGameDev <166923450+DarrenGohGameDev@users.noreply.github.com>
2026-02-28 13:03:59 +0800

    Added Entity base class

---
+++ EntityTemplate.cpp
@@ -0,0 +1,24 @@
+#include "EntityTemplate.h"
+
+EntityTemplate::EntityTemplate()
+{
+	currentLifeTime = baseLifeTime;
+	active = true;
+}
+
+bool EntityTemplate::GetEntityActiveStatus()
+{
+	return active;
+}
+
+void EntityTemplate::EntityUpdate(float deltaTime)
+{
+	if (isPlayer)
+		return;
+
+	currentLifeTime -= deltaTime;
+	if (currentLifeTime <= 0)
+	{
+		active = false;
+	}
+}
---
+++ EntityTemplate.h
@@ -0,0 +1,21 @@
+#pragma once
+class EntityTemplate
+{
+public:
+
+	EntityTemplate();
+
+	float baseLifeTime = 10.0f;
+
+	float currentLifeTime = 0.0f;
+
+	bool isPlayer = false;
+
+	bool GetEntityActiveStatus();
+
+	void EntityUpdate(float deltaTime);
+
+private:
+	bool active;
+};
+
